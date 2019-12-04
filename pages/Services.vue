1b67a91817b611f8253dce9395849a67f490aeb6
Nnamdi Kingsley umeh <endeking@gmail.com>
2019-12-04 16:19:57 +0100

    finished service page for mobile

--- pages/Services.vue
+++ pages/Services.vue
@@ -43,6 +43,225 @@
         </div>
       </div>
     </div>
+
+    <div id="software-wrapper">
+      <div class="container">
+        <div class="left">
+          <img src="~/assets/img/business-consulting.png">
+        </div>
+        <div class="right">
+          <h2 class="intro"> Software Solutions </h2>
+          <p class="content">
+            <span>
+            In the business of process automation, success is guaranteed by a businesses ability to manipulate software and hardware for the faster completion of tasks and processes. Another word for this is Innovation
+            </span>
+            <span>
+            We offer off the shelf as well as tailored software solutions for clients in order to attain the below golden eggs
+            </span>
+            <br>
+            <span>
+            — Competitive advantage over competitors
+            </span>
+            <span>
+            — Reduction in operative costs
+              </span>
+            <span>
+            —  Improved communication channels
+              </span>
+            <span>
+            — Enhanced visibility for stakeholders and customers
+              </span>
+            <span>
+            — Happier customers due to convenience and ease
+              </span>
+            <span>
+            — Improved ability to scale and grow of use
+          </span>
+          </p>
+        </div>
+      </div>
+    </div>
+
+    <div id="digital-wrapper">
+      <div class="container">
+        <div class="right">
+          <img src="~/assets/img/digital-services.png">
+        </div>
+        <div class="left">
+          <h2 class="intro"> Digital Design Solutions </h2>
+          <p class="content">
+            <span>Digital Media & Marketing. We also specialize in Content Aggregation and Content Creation. Our range of expertise include but is not limited to:</span>
+
+            <span>— Event coverage (Content Mining) </span>
+            <span>— Content Production </span>
+            <span>— Content management </span>
+            <span>— Graphics Design & Animation </span>
+            <span>— Advert production </span>
+            <span>— Script Writing </span>
+            <span>— Media Consultation </span>
+            <span>— Social Media Campaigns </span>
+          </p>
+
+        </div>
+      </div>
+    </div>
+
+    <div id="software-wrapper">
+      <div class="container">
+        <div class="left">
+          <img src="~/assets/img/software-services.png">
+        </div>
+        <div class="right">
+          <h2 class="intro"> Software Solutions </h2>
+          <p class="content">
+            <span>
+            In the business of process automation, success is guaranteed by a businesses ability to manipulate software and hardware for the faster completion of tasks and processes. Another word for this is Innovation
+            </span>
+            <span>
+            We offer off the shelf as well as tailored software solutions for clients in order to attain the below golden eggs
+            </span>
+            <br>
+            <span>
+            — Competitive advantage over competitors
+            </span>
+            <span>
+            — Reduction in operative costs
+              </span>
+            <span>
+            —  Improved communication channels
+              </span>
+            <span>
+            — Enhanced visibility for stakeholders and customers
+              </span>
+            <span>
+            — Happier customers due to convenience and ease
+              </span>
+            <span>
+            — Improved ability to scale and grow of use
+          </span>
+          </p>
+        </div>
+      </div>
+    </div>
+
+    <div id="digital-wrapper">
+      <div class="container">
+        <div class="right">
+          <img src="~/assets/img/advisory-service.png">
+        </div>
+        <div class="left">
+          <h2 class="intro"> Advisory Services </h2>
+          <p class="content">
+            <span>Digital Media & Marketing. We also specialize in Content Aggregation and Content Creation. Our range of expertise include but is not limited to:</span>
+
+            <span>— Event coverage (Content Mining) </span>
+            <span>— Content Production </span>
+            <span>— Content management </span>
+            <span>— Graphics Design & Animation </span>
+            <span>— Advert production </span>
+            <span>— Script Writing </span>
+            <span>— Media Consultation </span>
+            <span>— Social Media Campaigns </span>
+          </p>
+
+        </div>
+      </div>
+    </div>
+
+    <section id="testimonies" class="container">
+      <h3 class="sub">Working with innovate one company</h3>
+
+      <h1 class="intro">Hear what people that have worked with us have to say</h1>
+    </section>
+
+    <div id="features-wrapper" class="container">
+      <div class="feature blue">
+        <h1 class="intro">
+          Marketing Strategy Implementation
+        </h1>
+
+        <p class="content">
+          <span>We assist your company or organizations in creating a brand, increase its relevance, sell to a targeted market through online or offline channels, and then create brand loyalty through unique techniques. Below are services highlighted to accomplish this service:
+        </span>
+
+
+          <span>
+             Go-To-Market Strategy.
+          Brand Creation and Loyalty generation.
+          Market Segmentation and Deployment.
+          Content creation and distribution.
+            </span>
+        </p>
+      </div>
+      <div class="feature dark">
+        <h1 class="intro">
+          Marketing Strategy Implementation
+        </h1>
+
+        <p class="content">
+          <span>We assist your company or organizations in creating a brand, increase its relevance, sell to a targeted market through online or offline channels, and then create brand loyalty through unique techniques. Below are services highlighted to accomplish this service:
+        </span>
+
+
+          <span>
+             Go-To-Market Strategy.
+          Brand Creation and Loyalty generation.
+          Market Segmentation and Deployment.
+          Content creation and distribution.
+            </span>
+        </p>
+      </div>
+      <div class="feature pink">
+        <h1 class="intro">
+          Marketing Strategy Implementation
+        </h1>
+
+        <p class="content">
+          <span>We assist your company or organizations in creating a brand, increase its relevance, sell to a targeted market through online or offline channels, and then create brand loyalty through unique techniques. Below are services highlighted to accomplish this service:
+        </span>
+
+
+          <span>
+             Go-To-Market Strategy.
+          Brand Creation and Loyalty generation.
+          Market Segmentation and Deployment.
+          Content creation and distribution.
+            </span>
+        </p>
+      </div>
+      <div class="feature yellow">
+        <h1 class="intro">
+          Marketing Strategy Implementation
+        </h1>
+
+        <p class="content">
+          <span>We assist your company or organizations in creating a brand, increase its relevance, sell to a targeted market through online or offline channels, and then create brand loyalty through unique techniques. Below are services highlighted to accomplish this service:
+        </span>
+
+
+          <span>
+             Go-To-Market Strategy.
+          Brand Creation and Loyalty generation.
+          Market Segmentation and Deployment.
+          Content creation and distribution.
+            </span>
+        </p>
+      </div>
+    </div>
+
+    <div id="work" class="container">
+      <h1 class="title">Let's get to work</h1>
+
+      <p class="sub">
+        Have an unsolvable problem or an audacious idea?
+      </p>
+      <span class="subtitle">
+          We'd love to hear about it.
+        </span>
+
+
+      <a href="#" class="contact"> Contact us </a>
+    </div>
   </section>
 </template>
 
