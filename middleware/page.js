09e2368d3f52bd79942d51c3d64fb12eea24bdea
Yunus AbdulQudus <yunusabdulqudus1@gmail.com>
2020-02-25 18:23:00 +0100

    Adding images back

--- middleware/page.js
+++ middleware/page.js
@@ -1,7 +1,7 @@
 export default function (context) {
   //checks if a requested page exists
   const pages = require("~/assets/pages.json");
-  const page = pages.filter(page => page.name === context.route.params.id)
+  const page = pages.filter(page => page.name === context.route.params.id);
   if (page.length <= 0) {
     context.redirect('/products');
   }
